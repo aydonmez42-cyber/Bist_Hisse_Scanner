373e8f52b9b08217848db2ed042093022bdcac7d
aydonmez42-cyber <aydonmez42@gmail.com>
2026-09-12 14:50:04 +0300

    Delete logger.py

--- logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-def get_logger(name="bot"):
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s | %(levelname)s | %(message)s"
-    )
-    return logging.getLogger(name)
